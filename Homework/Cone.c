0dd8ccd4af8260215194852f02126f35ccc8d543
AndrewTroyan <andandrew1333@gmail.com>
2014-11-27 11:46:59 +0300

    Homework5

--- Homework/Cone.c
+++ Homework/Cone.c
@@ -8,5 +8,7 @@ int main()
     scanf("%d", &r);
     printf("Enter the height: ");
     scanf("%d", &h);
+    printf("V=%.2f\n", 3.14*r*r*h/3);
+    printf("S=%.2f\n", 3.14*r*r+sqrt(r*r+h*h));
     return 0;
 }
