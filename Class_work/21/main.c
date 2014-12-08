fb03644a6bc03e5ed83b0d480a61fede5c1f21f9
andrewtroyan <andandrew1333@gmail.com>
2014-12-08 12:09:16 +0300

    Classwork

---
+++ Class_work/21/main.c
@@ -0,0 +1,89 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    srand(time(NULL));
+    int humanScore = 0, computerScore = 0;
+    do {
+        humanScore = rand()%11+1+rand()%11+1;
+        computerScore = rand()%11+1+rand()%11+1;
+    } while (computerScore > 21 || humanScore > 21);
+    //---
+    printf("Your score is:\n%d\n", humanScore);
+    char answer = 'y';
+    if (humanScore > 11) {
+        printf("Would you like take another card? (y/n):\n");
+        scanf("%c", &answer);
+        if ('y' == answer) {
+            humanScore+=rand()%11+1;
+        }
+    }
+    //---
+    int computerWillTake=1;
+    if (computerScore > 11) {
+        computerWillTake = 21 - computerScore < rand()%21;
+    }
+    if (computerWillTake) {
+        computerScore = rand()%11+1;
+    }
+    //---
+    while (answer == 'y' && computerWillTake && humanScore < 21 && computerScore < 21) {
+        // повторяем (лучше использовать do while)
+        printf("Your score is:\n%d\n", humanScore);
+        char answer = 'y';
+        if (humanScore > 11) {
+            printf("Would you like take another card? (y/n):\n");
+            scanf("%c", &answer);
+            if ('y' == answer) {
+                humanScore+=rand()%11+1;
+            }
+        }
+    //---
+        int computerWillTake=1;
+        if (computerScore > 11) {
+            computerWillTake = 21 - computerScore < rand()%21;
+        }
+        if (computerWillTake) {
+            computerScore += rand()%11+1;
+        }
+    }
+    while (answer == 'y' && humanScore < 21) {
+        printf("Your score is:\n%d\n", humanScore);
+        answer = 'y';
+        if (humanScore > 11) {
+            printf("Would you like take another card? (y/n):\n");
+            scanf("%c", &answer);
+            if ('y' == answer) {
+                humanScore+=rand()%11+1;
+            }
+        }
+    }
+    while (computerWillTake && computerScore < 21) {
+        computerWillTake=1;
+        if (computerScore > 11) {
+            computerWillTake = 21 - computerScore < rand()%21;
+        }
+        if (computerWillTake) {
+            computerScore += rand()%11+1;
+        }
+    }
+    //---
+    if (humanScore == 21) {
+        printf("You win!\nYour score %d", humanScore);
+    }
+    else if (humanScore > 21) {
+        printf("Game over!\n");
+    }
+    else if (computerScore <= 21 && humanScore < computerScore) {
+        printf("Game over!\n");
+    }
+    else if (computerScore == humanScore) {
+        printf("Draw!\n");
+    }
+    else {
+        printf("You win!\n");
+    }
+
+}
---
+++ Class_work/Zmeyka.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    printf("%d\n", a);
+    printf(" %d\n", a+1);
+    printf("  %d\n", a+2);
+    printf(" %d\n", a+3);
+    printf("%d\n", a+4);
+    printf(" %d\n", a+5);
+    printf("  %d\n", a+6);
+    printf(" %d\n", a+7);
+    printf("%d\n", a+8);
+
+    return 0;
+}
