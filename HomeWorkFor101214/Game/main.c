6c4d7e7f9c03b0567357da611b4823193df07939
andrewtroyan <andandrew1333@gmail.com>
2014-12-09 16:04:16 +0300

    Homework

---
+++ HomeWorkFor101214/Game/main.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    printf("Hello!\n");
+    int computerNumber;
+    char answer;
+    do
+    {
+        srand(time(NULL));
+        computerNumber = rand()%99+1;
+        int userNumber = 0, shot = 0;                      //shot - попытка
+        printf("Enter the number from 1 to 99:\n");
+        while (userNumber != computerNumber)
+        {
+            scanf("%d", &userNumber);
+            if(userNumber > computerNumber)
+            {
+                printf("Too big!\n");
+                ++shot;
+            }
+            else if(userNumber < computerNumber)
+            {
+                printf("Too little!\n");
+                ++shot;
+            }
+        }
+        ++shot;
+        printf("You won! You tried for %d time(s). Do you want to play again? (y/n):\n", shot);
+        scanf("%c", &answer);
+    } while(answer == 'y');
+    printf("Goodbye!");
+    return 0;
+}
+
+
+
+
+
