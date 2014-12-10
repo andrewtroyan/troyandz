5c36f322cad80eafbfaebca371f198326c75cfd1
andrewtroyan <andandrew1333@gmail.com>
2014-12-10 23:07:50 +0300

    GreatCommonDevisor

---
+++ HomeWork/HomeWorkFor151214/GreatCommonDevisor/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1, num2, max, min, minNew;
+    printf("Enter the first number: ");
+    scanf("%d", &num1);
+    printf("Enter the first number: ");
+    scanf("%d", &num2);
+    max = num1 > num2? num1 : num2;
+    min = num2 > num1? num1 : num2;
+    do
+    {
+        minNew = max%min;
+        max = min;
+        min = minNew;
+    } while (minNew != 0);
+    printf("%d", max);
+    return 0;
+}
+
+
