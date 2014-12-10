4f31453180ab63d54f07874cfce2433c3d2dedeb
andrewtroyan <andandrew1333@gmail.com>
2014-12-10 15:35:49 +0300

    Classwork

--- ClassWork/Game/main.c
+++ ClassWork/Game/main.c
@@ -10,27 +10,24 @@ int main()
     do
     {
         printf("\nImagine the number between 1 and 99.\n");
-        int computerNumber;
+        int computerNumber, startNumber = 1, endNumber = 99;
         srand(time(NULL));
-        computerNumber = rand()%99+1;
-        printf("%d\n", computerNumber);
-        printf("Am I right? (-1/0/1)\n");
-        scanf("%d", &answerInt);
-        while (answerInt != 0)
+        do
         {
-            if(answerInt == -1)
-            {
-                computerNumber = rand()%(99-computerNumber+1)+computerNumber;
-            }
-            else if(answerInt == 1)
-            {
-                computerNumber = rand()%computerNumber+1;
-            }
+            computerNumber = rand()%(endNumber - startNumber + 1) + startNumber;
             printf("%d\n", computerNumber);
             printf("Am I right? (-1/0/1)\n");
             scanf("%d", &answerInt);
-        }
-        printf("Congratulations!\nWanna play again with me? (y/n):\n");
+            if(answerInt == -1)
+            {
+                startNumber = computerNumber + 1;
+            }
+            else if(answerInt == 1)
+            {
+                endNumber = computerNumber - 1;
+            }
+        } while(answerInt != 0);
+        printf("\nCongratulations!\nWanna play again with me? (y/n):\n");
         scanf(" %c", &answerChar);
     } while(answerChar == 'y');
     printf("\nIt was pleasant to play with you! Goodbye!\n");
