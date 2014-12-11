e21075112fa2b855b2369e604ebd902bb7d88944
andrewtroyan <andandrew1333@gmail.com>
2014-12-11 19:08:25 +0300

    Homework

---
+++ HomeWork/HomeWorkFor151214/Frame/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int width, height;
+    printf("Enter the width (>= 1): ");
+    scanf("%d", &width);
+    printf("Enter the height (>= 1): ");
+    scanf("%d", &height);
+    for(int i = 1; i<= height; ++i)
+    {
+        if (i == 1 || i == height)
+        {
+            for(int k = 1; k <= width; ++k)
+            {
+                if (k == width)
+                {
+                    printf("%c\n", '#');
+                }
+                else
+                {
+                    printf("%c", '#');
+                }
+            }
+        }
+        else
+        {
+            for(int k = 1; k <= width; ++k)
+            {
+                if(k == 1)
+                {
+                    printf("%c", '#');
+                }
+                else if(k == width)
+                {
+                    printf("%c\n", '#');
+                }
+                else
+                {
+                    printf("%c", ' ');
+                }
+            }
+        }
+    }
+    return 0;
+}
