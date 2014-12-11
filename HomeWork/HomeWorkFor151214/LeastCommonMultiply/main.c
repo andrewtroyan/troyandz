0c49308f574f1989dcf4a1c974d6f23876ed77a0
andrewtroyan <andandrew1333@gmail.com>
2014-12-11 07:25:18 +0300

    Homework

--- HomeWork/HomeWorkFor151214/LeastCommonMultiply/main.c
+++ HomeWork/HomeWorkFor151214/LeastCommonMultiply/main.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-    int num1, num2, max, min, excess;
+    int num1, num2, max, min, excess, excess2, max2;
     printf("Enter the first number: ");
     scanf("%d", &num1);
     printf("Enter the second number: ");
@@ -16,8 +16,8 @@ int main()
         max = min;
         min = excess;
     } while (excess != 0);
-    printf("The great common devisor of these numbers is %d.", max);
+    excess2 = num1 / max;
+    max2 = excess2 * num2;
+    printf("The least common multiply of these numbers is %d.", max2);
     return 0;
 }
-
-
