d2bf71c682399858fde1d93523a8a84cd7f864cb
andrewtroyan <andandrew1333@gmail.com>
2014-12-15 16:45:55 +0300

    Homework

---
+++ HomeWork/HomeWorkFor171214/PrimeNumber/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int number, indicator = 0;
+    printf("Enter the number: ");
+    scanf("%d", &number);
+    for (int i = 2; i < number; ++i)
+    {
+        if(number%i == 0)
+        {
+            indicator = 1;
+        }
+    }
+    if (indicator)
+    {
+        printf("The number is not prime.");
+    }
+    else
+    {
+        printf("The number is prime.");
+    }
+    return 0;
+}
