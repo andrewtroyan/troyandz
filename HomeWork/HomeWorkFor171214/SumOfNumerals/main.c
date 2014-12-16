fd77a6f66e2e1870bad00d7f77e2f71953257209
andrewtroyan <andandrew1333@gmail.com>
2014-12-16 19:04:36 +0300

    Homework

--- HomeWork/HomeWorkFor171214/SumOfNumerals/main.c
+++ HomeWork/HomeWorkFor171214/SumOfNumerals/main.c
@@ -6,19 +6,10 @@ int main()
     int number, sumOfNumerals = 0;
     printf("Enter the number: ");
     scanf("%d", &number);
-    int decades = 1;
-    while (decades <= number)
-    {
-        decades *= 10;
-    }
     while (number)
     {
-        while(decades > number)
-        {
-            decades /= 10;
-        }
-        sumOfNumerals += number/decades;
-        number %= decades;
+        sumOfNumerals += number % 10;
+        number /= 10;
     }
     printf("The sum of numerals is %d.", sumOfNumerals);
     return 0;
