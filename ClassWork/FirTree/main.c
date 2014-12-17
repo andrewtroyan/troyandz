629e5b3de8c015690fb4a21038779dbc8514173e
andrewtroyan <andandrew1333@gmail.com>
2014-12-17 13:43:37 +0300

    Fir-tree done

--- ClassWork/FirTree/main.c
+++ ClassWork/FirTree/main.c
@@ -6,12 +6,15 @@ int main()
     int number;
     printf("Enter the number: ");
     scanf("%d", &number);
-    int amountOfSpacesLeft = number + 1, amountOfSpacesBetween = 0;
-    for(int i = 0; i <= number; ++i)
+    for(int k = 1; k <= number; ++k)
     {
-        printf("%*c%*s%c\n", amountOfSpacesLeft, '*', amountOfSpacesBetween, "", '*');
-        --amountOfSpacesLeft;
-        amountOfSpacesBetween += 2;
+        int amountOfSpacesLeft = number + 1, amountOfSpacesBetween = 0;
+        for(int i = 0; i <= k; ++i)
+        {
+            printf("%*c%*s%c\n", amountOfSpacesLeft, '*', amountOfSpacesBetween, "", '*');
+            --amountOfSpacesLeft;
+            amountOfSpacesBetween += 2;
+        }
     }
     return 0;
 }
