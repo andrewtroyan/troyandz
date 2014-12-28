6887420eedc40a290ee0034b0610636d4f2ab6ab
andrewtroyan <andandrew1333@gmail.com>
2014-12-28 17:02:33 +0200

    Homework

---
+++ HomeWork/HomeWorkDuringTheHolidays/Level02/Task01/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int amountOfLayers;
+    printf("Enter the amount of layers: ");
+    scanf("%d", &amountOfLayers);
+    for (int i = 1, k = 1; i <= amountOfLayers; ++i, ++k)
+    {
+        printf ("%*s%c\n", amountOfLayers, "", '*');
+        int amountOfSpacesLeft = amountOfLayers - 1, amountOfSpacesBetween = 1;
+        for (int l = 2; l <= i; ++l)
+        {
+            printf("%*s%c%*s%c\n", amountOfSpacesLeft, "", '*', amountOfSpacesBetween, "", '*');
+            --amountOfSpacesLeft;
+            amountOfSpacesBetween += 2;
+        }
+        printf("%*s", amountOfLayers - k, "");
+        for (int j = 1; j <= (i * 2 + 1); ++j)
+        {
+            printf("%c", '*');
+        }
+        printf("\n");
+    }
+    if (amountOfLayers <= 3)
+    {
+        printf("%*s%c\n%*s%c", amountOfLayers, "", '*', amountOfLayers, "", '*');
+    }
+    else
+    {
+        printf("%*s%c%2c\n%*s%c%2c", amountOfLayers - 1, "", '*', '*', amountOfLayers - 1, "", '*', '*');
+    }
+    return 0;
+}
