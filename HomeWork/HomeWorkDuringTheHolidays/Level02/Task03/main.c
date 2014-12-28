e08836bef71dcdf8dba9cb4bfeb3887cf59b6bd0
andrewtroyan <andandrew1333@gmail.com>
2014-12-29 00:41:15 +0200

    Update

--- HomeWork/HomeWorkDuringTheHolidays/Level02/Task03/main.c
+++ HomeWork/HomeWorkDuringTheHolidays/Level02/Task03/main.c
@@ -17,42 +17,14 @@ int main()
         duoDecimalNumber = decimalNumber / indicator;
         switch (duoDecimalNumber)
         {
-        case 0:
-            printf("0");
-            break;
-        case 1:
-            printf("1");
-            break;
-        case 2:
-            printf("2");
-            break;
-        case 3:
-            printf("3");
-            break;
-        case 4:
-            printf("4");
-            break;
-        case 5:
-            printf("5");
-            break;
-        case 6:
-            printf("6");
-            break;
-        case 7:
-            printf("7");
-            break;
-        case 8:
-            printf("8");
-            break;
-        case 9:
-            printf("9");
-            break;
         case 10:
             printf("A");
             break;
         case 11:
             printf("B");
             break;
+        default:
+            printf("%d", duoDecimalNumber);
         }
         decimalNumber %= indicator;
         indicator /= 12;
