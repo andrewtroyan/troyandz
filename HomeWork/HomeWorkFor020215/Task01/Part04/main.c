dc5f184158ef57555b7acd68e2a381f60ef7338f
andrewtroyan <andandrew1333@gmail.com>
2015-01-30 19:46:03 +0300

    Homework

---
+++ HomeWork/HomeWorkFor020215/Task01/Part04/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "../../../../modules/include/array.h"
+#include "../../../../modules/include/compatibility.h"
+#include "../askForGenerateOrWriting.h"
+
+int main()
+{
+    int array[SIZE], sizeOfPart, maxElement, minElement;
+    printf("Enter the size of array (0 < size <= %d): ", SIZE);
+    scanf("%d", &sizeOfPart);
+    askForGenerateOrWriting(array, sizeOfPart);
+    universalClear();
+    printf("Your array:\n");
+    outputArrray(array, sizeOfPart);
+    maxElement = maxOfArray(array, sizeOfPart);
+    minElement = minOfArray(array, sizeOfPart);
+    for(int i = 0; i < sizeOfPart; ++i)
+    {
+        array[i] = array[i] % 2? minElement : maxElement;
+    }
+    printf("New array:\n");
+    outputArrray(array, sizeOfPart);
+    return 0;
+}
