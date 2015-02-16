7a1125fd8aa5713b593d2ccd56fe2202868cf09a
andrewtroyan <andandrew1333@gmail.com>
2015-02-16 10:32:15 +0000

    Classwork

---
+++ ClassWork/NcursesExample/main.c
@@ -0,0 +1,60 @@
+#define _XOPEN_SOURCE_EXTENDED //отвечают за
+#define _POSIX_C_SOURCE 199309L //работу с юникодом
+
+#include <sys/ioctl.h> //отвечает за получение размеров консоли
+#include <signal.h> //отвечает за взаимодействие с ОС в виде реагирования каких-либо сигналов
+#include <stdio.h>
+#include <stdlib.h>
+#include <ncurses.h>
+#include <locale.h> //нужна, чтобы всё грамотно работало с юникодом
+
+void treatSigWinch(int signo);
+void initialiseProgram();
+enum Colors{normal, green, red};
+
+int main()
+{
+    initialiseProgram();
+
+    attron(COLOR_PAIR(1)); //attribute on, активируем цветовую пару
+    printw("Hello world!");
+    attroff(COLOR_PAIR(1));
+    refresh();
+    getch();
+    attron(A_BLINK|A_BOLD);
+    move(0, 0);
+    printw("Hello");
+    attroff(A_BLINK|A_BOLD);
+    refresh();
+    getch();
+    endwin();
+    return 0;
+}
+
+void initialiseProgram()
+{
+    setlocale(LC_ALL, "");
+    initscr(); //начать экран
+    signal(SIGWINCH, treatSigWinch); //назначаем сигналу sigwinch обработчик
+    if(has_colors() == FALSE)
+    {
+        endwin();
+        fprintf(stderr, "no colors\n");
+        exit(1);
+    }
+    cbreak();
+    noecho();
+    curs_set(0);
+    start_color(); //"начинаем" цвета
+    init_pair(1, COLOR_GREEN, COLOR_BLACK); //зеленый шрифт, черный фон
+    //init_pair(normal, COLOR_WHITE, COLOR_BLACK);
+    //init_pair(green, COLOR_GREEN, COLOR_BLACK);
+    //init_pair(red, COLOR_RED, COLOR_BLACK);
+}
+
+void treatSigWinch(int signo)
+{
+    struct winsize size;
+    ioctl(fileno(stdout), TIOCGWINSZ, (char *) &size);
+    resizeterm(size.ws_row, size.ws_col);
+}
