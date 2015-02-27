e5a584cce0e2f61c740556ed3109caffc849ec11
andrewtroyan <andandrew1333@gmail.com>
2015-02-27 22:18:49 +0300

    Correction

--- HomeWork/HomeWorkFor230215/TicTacToe3D/TicTacToe3D.c
+++ HomeWork/HomeWorkFor230215/TicTacToe3D/TicTacToe3D.c
@@ -3,35 +3,6 @@
 static char unusedCell[4] = "\342\227\206";
 static int currentLayer = 0, currentI = 0, currentJ = 0, upDown = 0, leftRight = 0;
 
-void treatSigWinch(int signo)
-{
-    struct winsize size;
-    ioctl(fileno(stdout), TIOCGWINSZ, (char *) &size);
-    resizeterm(size.ws_row, size.ws_col);
-}
-
-void initialiseProgram()
-{
-    setlocale(LC_ALL, "");
-    initscr();
-    signal(SIGWINCH, treatSigWinch);
-    if(has_colors() == FALSE)
-    {
-        endwin();
-        fprintf(stderr, "no colors\n");
-        exit(1);
-    }
-    cbreak();
-    noecho();
-    curs_set(0);
-    start_color();
-    init_pair(normal, COLOR_WHITE, COLOR_BLACK);
-    init_pair(green, COLOR_GREEN, COLOR_BLACK);
-    init_pair(red, COLOR_RED, COLOR_BLACK);
-    init_pair(blue, COLOR_BLUE, COLOR_BLACK);
-    init_pair(yellow, COLOR_YELLOW, COLOR_BLACK);
-}
-
 void drawField(int ***field)
 {
     int up = 0, down = 0;
--- HomeWork/HomeWorkFor230215/TicTacToe3D/TicTacToe3D.h
+++ HomeWork/HomeWorkFor230215/TicTacToe3D/TicTacToe3D.h
@@ -1,27 +1,12 @@
 #ifndef BIGTICTACTOE_H_INCLUDED
 #define BIGTICTACTOE_H_INCLUDED
 
-#define _XOPEN_SOURCE_EXTENDED
-#define _POSIX_C_SOURCE 199309L
-
-#include <sys/ioctl.h>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <ncurses.h>
-#include <locale.h>
-#include <stdbool.h>
-#include <time.h>
-#include <assert.h>
+#include "../../../modules/include/ncursesDetails.h"
 
 #define LAYERS 4
 #define ROWS 4
 #define COLS 4
 
-enum Colors {normal, green, red, blue, yellow};
-
-void treatSigWinch(int signo);
-void initialiseProgram();
 void drawField(int ***field);
 void playTheGame(int ***field);
 int checkTheGame(int ***field);
