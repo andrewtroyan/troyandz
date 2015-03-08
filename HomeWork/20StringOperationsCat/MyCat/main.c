4d11868113eff8ef65ff1fd42c875b5775b33b8e
andrewtroyan <andandrew1333@gmail.com>
2015-03-08 15:51:22 +0300

    Homework

--- HomeWork/20StringOperationsCat/MyCat/main.c
+++ HomeWork/20StringOperationsCat/MyCat/main.c
@@ -6,27 +6,63 @@
 
 int main(int argc, char **argv)
 {
+    FILE *filePointer;
+    char strTemp[SIZE];
+    int counter = 1;
+
     if(argc > 1)
     {
         if(strcmp(argv[1], "--number") == 0 || strcmp(argv[1], "-n") == 0)
         {
-            FILE *filePoint;
-            char strTemp[SIZE];
-            int counter = 1;
             for(int i = 2; argv[i] != NULL; ++i)
             {
-                filePoint = fopen(argv[i], "r");
-                if(filePoint == NULL)
+                filePointer = fopen(argv[i], "r");
+                if(filePointer == NULL)
                 {
-                    fprintf(stderr, "No such file or directory.\n");
-                    exit(1);
+                    fprintf(stderr, "%s: No such file or directory.\n", argv[i]);
                 }
-                while(fgets(strTemp, sizeof(strTemp), filePoint))
+                else
                 {
-                    printf("%6d  %s", counter, strTemp);
-                    ++counter;
+                    while(fgets(strTemp, sizeof(strTemp), filePointer))
+                    {
+                        printf("%6d  %s", counter, strTemp);
+                        ++counter;
+                    }
+                    fclose(filePointer);
                 }
-                fclose(filePoint);
+            }
+        }
+        else if(strcmp(argv[1], "--help") == 0)
+        {
+            filePointer = fopen("/home/troyan/andrewtroyan/troyandz/HomeWork/20StringOperationsCat/MyCat/CatInstructions.txt", "r");
+            if(filePointer == NULL)
+            {
+                fprintf(stderr, "Access error.\n");
+                exit(1);
+            }
+            while(fgets(strTemp, sizeof(strTemp), filePointer))
+            {
+                puts(strTemp);
+            }
+            fclose(filePointer);
+        }
+        else
+        {
+            for(int i = 1; argv[i] != NULL; ++i)
+            {
+                filePointer = fopen(argv[i], "r");
+                if(filePointer == NULL)
+                {
+                    fprintf(stderr, "%s: No such file or directory.\n", argv[i]);
+                }
+                else
+                {
+                    while(fgets(strTemp, sizeof(strTemp), filePointer))
+                    {
+                        printf("%s", strTemp);
+                    }
+                    fclose(filePointer);
+                }
             }
         }
     }
