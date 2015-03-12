b8435d4211fc699710c5032635a8ed6b8f64f048
andrewtroyan <andandrew1333@gmail.com>
2015-03-12 19:47:04 +0300

    Homework

--- HomeWork/21Structures/Time/main.c
+++ HomeWork/21Structures/Time/main.c
@@ -2,38 +2,52 @@
 
 int main()
 {
-    Time commonTime = {0, 0, 0}, firstTime, secondTime;
+    Time currentTime, enteredTime = {0, 0, 0}, firstTime, secondTime;
     int answer;
     long int seconds, result;
 
+    time_t rawtime;
+    struct tm *timeinfo;
+
     printf("Instruction\nEnter the time in this order:\nHOURS:MINUTES:SECONDS.\n\n"
-           "Select an operation:\n1. Enter\n2. Show\n3. Add seconds to the time\n4. Difference between two moments of time in seconds\n5. Exit\n\n");
+           "Select an operation:\n1. Enter\n2. Show time entered by you\n3. Show current time\n4. Add seconds to the time entered by you\n5. Difference between two moments of time in seconds\n6. Exit\n\n");
 
     scanf("%d", &answer);
-    while(answer > 0 && answer < 5)
+    while(answer > 0 && answer < 6)
     {
         system("clear");
         printf("Instruction\nEnter the time in this order:\nHOURS:MINUTES:SECONDS.\n\n"
-               "Select an operation:\n1. Enter\n2. Show\n3. Add seconds to the time\n4. Difference between two moments of time in seconds\n5. Exit\n\n");
+               "Select an operation:\n1. Enter\n2. Show time entered by you\n3. Show current time\n4. Add seconds to the time entered by you\n5. Difference between two moments of time in seconds\n6. Exit\n\n");
 
         switch(answer)
         {
         case 1:
             printf("Enter the time: ");
-            enterTime(&commonTime);
+            enterTime(&enteredTime);
             break;
         case 2:
             printf("Your entered time: ");
-            showTime(commonTime);
+            showTime(enteredTime);
             break;
         case 3:
-            printf("Enter the amount of seconds that you want to add to your last entered time: ");
-            scanf("%d", &seconds);
-            addSeconds(&commonTime, seconds);
-            printf("The result: ");
-            showTime(commonTime);
+            time (&rawtime);
+            timeinfo = localtime (&rawtime);
+
+            currentTime.hours = timeinfo->tm_hour;
+            currentTime.minutes = timeinfo->tm_min;
+            currentTime.seconds = timeinfo->tm_sec;
+
+            printf("The current time: ");
+            showTime(currentTime);
             break;
         case 4:
+            printf("Enter the amount of seconds that you want to add to your last entered time: ");
+            scanf("%d", &seconds);
+            addSeconds(&enteredTime, seconds);
+            printf("The result: ");
+            showTime(enteredTime);
+            break;
+        case 5:
             printf("Enter the first time: ");
             enterTime(&firstTime);
             printf("Enter the second time: ");
