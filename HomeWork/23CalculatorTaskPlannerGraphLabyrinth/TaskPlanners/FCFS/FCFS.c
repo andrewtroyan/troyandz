f64aaaa67ee57bc5c6ad3c8ad01c1f46348b8c5a
andrewtroyan <andandrew1333@gmail.com>
2015-03-21 00:59:48 +0300

    Homework

---
+++ HomeWork/23CalculatorTaskPlannerGraphLabyrinth/TaskPlanners/FCFS/FCFS.c
@@ -0,0 +1,163 @@
+#include "FCFS.h"
+
+// ---QUEUE---
+
+void initializeQueue(Queue *pointer)
+{
+    pointer->head = NULL;
+    pointer->tail = NULL;
+}
+
+bool addToQueue(Process data, Queue *pointer)
+{
+    RunningNode *temp = NULL;
+    temp = (RunningNode *)malloc(sizeof(RunningNode));
+    if(temp == NULL)
+    {
+        return false;
+    }
+    temp->process = data;
+    temp->link = NULL;
+
+    if(!(pointer->head))
+    {
+        pointer->head = temp;
+        pointer->tail = temp;
+    }
+    else
+    {
+        pointer->tail->link = temp;
+        pointer->tail = temp;
+    }
+
+    temp = NULL;
+    return true;
+}
+
+void popFromQueue(Queue *pointer)
+{
+    if(pointer->head)
+    {
+        RunningNode *temp = pointer->head;
+        pointer->head = pointer->head->link;
+        temp->process.identifier = 0;
+        temp->process.comingTime = 0;
+        temp->process.runningTime = 0;
+        temp->link = NULL;
+        free(temp);
+        temp = NULL;
+
+        if(!(pointer->head))
+            pointer->tail = NULL;
+    }
+}
+
+bool isQueueEmpty(Queue pointer)
+{
+    return !pointer.head;
+}
+
+void onHeadOfQueue(Process *process, Queue pointer)
+{
+    if(pointer.head)
+        *process = pointer.head->process;
+}
+
+// ---LISTS---
+
+bool addToListOfUnbornProcesses(Process data, ListOfUnbornProcesses *pointer)
+{
+    RunningNode *temp1 = NULL, *temp2 = *pointer;
+    temp1 = (RunningNode *)malloc(sizeof(RunningNode));
+    if(temp1 == NULL)
+        return false;
+    temp1->process = data;
+    if(temp2 == NULL)
+    {
+        temp1->link = NULL;
+        *pointer = temp1;
+    }
+    else if(temp2->process.comingTime > temp1->process.comingTime)
+    {
+        temp1->link = temp2;
+        *pointer = temp1;
+    }
+    else
+    {
+        while(temp2->link && temp2->link->process.comingTime < temp1->process.comingTime)
+            temp2 = temp2->link;
+        temp1->link = temp2->link;
+        temp2->link = temp1;
+    }
+    return true;
+}
+
+void deleteFromListOfUnbornProcesses(ListOfUnbornProcesses *pointer)
+{
+    if(*pointer)
+    {
+        RunningNode *temp = *pointer;
+        *pointer = (*pointer)->link;
+        temp->process.identifier = 0;
+        temp->process.runningTime = 0;
+        temp->process.comingTime = 0;
+        temp->link = NULL;
+        free(temp);
+        temp = NULL;
+    }
+}
+
+void onHeadOfUnbornProcessesList(Process *process, ListOfUnbornProcesses pointer)
+{
+    if(pointer)
+        *process = pointer->process;
+}
+
+// ---
+
+void initializeListOfFinishedProcesses(ListOfFinishedProcesses *pointer)
+{
+    pointer->node = NULL;
+    pointer->amountOfFinishedProcesses = 0;
+}
+
+bool addToListOfFinishedProcesses(FinishedProcess data, ListOfFinishedProcesses *pointer)
+{
+    FinishedNode *newData = NULL, *tempPointer = pointer->node;
+
+    newData = (FinishedNode *)malloc(sizeof(FinishedNode));
+    if(newData == NULL)
+        return false;
+    newData->process = data;
+    newData->link = NULL;
+
+    if(tempPointer == NULL)
+        pointer->node = newData;
+    else
+    {
+        while(tempPointer->link)
+            tempPointer = tempPointer->link;
+        tempPointer->link = newData;
+    }
+
+    ++(pointer->amountOfFinishedProcesses);
+    newData = NULL;
+    return true;
+}
+
+void clearTheListOfFinishedProcesses(ListOfFinishedProcesses *pointer)
+{
+    FinishedNode *tempPointer = pointer->node;
+    pointer->amountOfFinishedProcesses = 0;
+    while(pointer->node)
+    {
+        tempPointer = pointer->node->link;
+        free(pointer->node);
+        pointer->node = tempPointer;
+    }
+}
+
+int amountOfFinishedProcesses(ListOfFinishedProcesses pointer)
+{
+    return pointer.amountOfFinishedProcesses;
+}
