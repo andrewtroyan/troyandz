b9f729270f1fde5d52d3bb866e07f160ace2c669
andrewtroyan <andandrew1333@gmail.com>
2015-04-13 12:22:53 +0000

    SDL

--- ClassWork/SDLwithRAII/sdlwrapper.h
+++ ClassWork/SDLwithRAII/sdlwrapper.h
@@ -14,6 +14,20 @@ class SDLWindowWrapper
 {
     public:
         SDLWindowWrapper(const char *title, int x, int y, int w, int h, Uint32 flags);
+        ~SDLWindowWrapper();
+        SDL_Window *win() const { return win_; }
+    private:
+        SDL_Window *win_;
+};
+
+class SDLRendererWrapper
+{
+public:
+    SDLRendererWrapper(SDL_Window *win, int index, Uint32 flags);
+    ~SDLRendererWrapper();
+    SDL_Renderer *ren() { return ren_; }
+private:
+    SDL_Renderer *ren_;
 };
 
 #endif // SDLWRAPPER_H
